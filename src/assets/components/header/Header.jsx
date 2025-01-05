4ca64ead6f6ff59058eb64f7564db47b192fcf5b
davidrt <jdramirezt@unal.edu.co>
2025-01-05 11:04:16 -0500

    initial commit

---
+++ src/assets/components/header/Header.jsx
@@ -0,0 +1,49 @@
+import { Link } from "react-router-dom";
+import LoginButton from "./LoginButton";
+import RegistroButton from "./RegistroButton";
+import { useAuth0 } from "@auth0/auth0-react";
+import LogoutButton from "./LogoutButton";
+import ProfileButton from "./ProfileButton";
+
+const Header = () => {
+    const { isAuthenticated } = useAuth0();
+
+    return (
+        <>
+            <div className="bg-white lg:pb-12">
+                <div className="px-4 mx-auto max-w-screen-2xl md:px-8">
+                    <header className="flex items-center justify-between py-4 md:py-8">
+                        {/* logo - start */}
+                        <Link
+                            to="/"
+                            className="text-black-800 inline-flex items-center gap-2.5 text-2xl font-bold md:text-3xl"
+                            aria-label="logo"
+                        >
+                            ASKy
+                        </Link>
+                        {/* logo - end */}
+                        {/* nav - start */}
+                        {/* nav - end */}
+                        {/* buttons - start */}
+                        <div className="-ml-8 flex-col gap-2.5 sm:flex-row sm:justify-center lg:flex lg:justify-start">
+                            {isAuthenticated ? (
+                                <>
+                                    <LogoutButton />
+                                    <ProfileButton />
+                                </>
+                            ) : (
+                                <>
+                                    <LoginButton />
+                                    <RegistroButton />
+                                </>
+                            )}
+                        </div>
+                        {/* buttons - end */}
+                    </header>
+                </div>
+            </div>
+        </>
+    );
+};
+
+export default Header;
