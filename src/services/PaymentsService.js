27acf302cb686669b787a1b3d37cc9334ce4f1e5
davidrt <jdramirezt@unal.edu.co>
2025-03-03 19:18:49 -0500

    payments services initial commit

---
+++ src/services/PaymentsService.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const EXPERT_BASE_RES_API_URL = "https://askybackend.onrender.com/payments";
+
+class PaymentsService {
+
+    // payments/expert/payout/status/{payoutID}
+    getPaymentsByPayoutID(payoutId) {
+        return axios.get(`${EXPERT_BASE_RES_API_URL}/expert/payout/status/${payoutId}`, {
+        });
+    }
+    //payments/withdraw
+    withdrawPayments() {
+        return axios.get(`${EXPERT_BASE_RES_API_URL}/withdraw`, {
+        });
+    }
+    //payments/recharge
+    rechargePayments() {
+        return axios.get(`${EXPERT_BASE_RES_API_URL}/withdraw`, {
+        });
+    }
+    //payments/expert
+    expertPayout() {
+        return axios.get(`${EXPERT_BASE_RES_API_URL}/withdraw`, {
+        });
+    }
+
+}
+
+// Asigna la instancia a una variable antes de exportarla
+const paymentsService = new PaymentsService();
+export default paymentsService;
